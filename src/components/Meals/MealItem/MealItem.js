15d6e6c94f835cb5ae8f9087f100be9a68f1ebdc
viktar-b <viktar.beliakou@gmail.com>
2021-05-03 18:05:26 +0100

    UI elements added

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 const MealItem = (props) => {
     //template literal
@@ -12,7 +13,9 @@ const MealItem = (props) => {
                 <div className={classes.description}>{props.description}</div>
                 <div className={classes.price}>{price}</div>
             </div>
-            <div></div>
+            <div>
+                <MealItemForm />
+            </div>
         </li>
     )
 }
