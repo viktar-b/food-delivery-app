63de048f2057776a6455d881dc6b4b3dd2bad865
viktar-b <viktar.beliakou@gmail.com>
2021-05-04 21:19:25 +0100

    Modal reducer added

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -7,12 +7,31 @@ const defaultCartState = {
 }
 
 const cartReducer = (state, action) => {
-    if (action.type == 'ADD') {
-        const udpatedItems = state.items.concat(action.item);
-        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    if (action.type === 'ADD') {
+        const updatedTotalAmount = 
+            state.totalAmount + action.item.price * action.item.amount;
+        
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.item.id
+        );
+        
+        const existingCartItem = state.items[existingCartItemIndex];
+        
+        let updatedItems;
+        if (existingCartItem) {
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            };
+
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+            updatedItems = state.items.concat(action.item);
+        }
 
         return {
-            items: udpatedItems,
+            items: updatedItems,
             totalAmount: updatedTotalAmount
         };
     }
@@ -21,7 +40,10 @@ const cartReducer = (state, action) => {
 
 const CartProvider = (props) => {
 
-    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+    const [cartState, dispatchCartAction] = useReducer(
+        cartReducer, 
+        defaultCartState
+    );
 
     const addItemToCartHandler = (item) => {
         dispatchCartAction({
