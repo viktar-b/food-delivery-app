49ffcf2c4d40a91fe3e55786a2c877afea5dbf3f
viktar-b <viktar.beliakou@gmail.com>
2021-05-05 12:54:59 +0100

    animation for cart is fixed

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,20 +1,49 @@
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 const HeaderCartButton = (props) => {
-
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false)
     const cartCtx = useContext(CartContext);
 
-    const numberOfCartItems = cartCtx.items.reduce(
+    //object destructuring
+    const { items } = cartCtx;
+
+    const numberOfCartItems = items.reduce(
         (curNumber, item) => {
             return curNumber + item.amount;
         }, 0
     );
 
+    const btnClasses = 
+        `${classes.button} ${btnIsHighlighted ? classes.bump : ' '}`;
+        
+    //used for handling sideffects 
+    useEffect(
+        () => {
+            if (items.length === 0) {
+                return;
+            }
+            setBtnIsHighlighted(true);
+
+            const timer = setTimeout(() => {
+                setBtnIsHighlighted(false);
+            }, 300); //300 for bump animation see css file
+
+            /* 
+             * to make sure animation is triggered everytime button is pressed
+             * timer is cleared  to allow newtimer to be set
+             */
+            return () => {
+                clearTimeout(timer);
+            };
+
+        }, [items]
+    ); 
+
     return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
